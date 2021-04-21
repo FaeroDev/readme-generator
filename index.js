7f04cd8c87132ac69fa1a6ac76f7e2fa3be5cccc
pharaohnof <pharaohnof@gmail.com>
2021-04-21 16:42:13 -0400

    require inquirer and fs - initial attempt at inquirer array

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the project title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Write a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How do you use your project',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your Email address.',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Enter contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Enter Testing instructions:',
+      },
+  ])
+
+  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
