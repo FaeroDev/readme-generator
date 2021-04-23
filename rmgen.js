553422efcc5e76ad18d4050c48aa237f781d41e7
Matthew Pawlak <pharaohnof@gmail.com>
2021-04-22 23:59:22 -0400

    inquirer working -  renamed file to rmgen.js for future shell scripting purpose, changed package.json to reflect new main

--- rmgen.js
+++ rmgen.js
@@ -48,7 +48,10 @@ inquirer
       },
   ])
 
-  
+.then((answers) => {
+  console.log(answers.title);
+
+})
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
